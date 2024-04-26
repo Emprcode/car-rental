63b26c7c77a64a6e4cef8da59f706f98ca10dbf9
Narayan Aryal <narayanaryal.code@gmail.com>
2024-04-26 21:24:57 +1000

    Created basic layout

---
+++ frontend/src/layout/header/Header.tsx
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+export const Header = ()=>  {
+  return (
+    <Navbar expand="md" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Car Rental</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
---
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { MainLayout } from '../../layout/mainLayout/MainLayout'
+import { Container } from 'react-bootstrap'
+
+const Dashboard = () => {
+  return (
+    <MainLayout>
+      <Container>
+        <div>
+          hey
+        </div>
+      </Container>
+    </MainLayout>
+  )
+}
+
+export default Dashboard
